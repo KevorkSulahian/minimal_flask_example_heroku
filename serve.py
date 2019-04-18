484107f0ec4eec13de8a466f33e8d1bbca3aa570
herooooooooo <kevolig1234@gmail.com>
2019-04-19 00:08:43 +0400

    Update serve.py

--- serve.py
+++ serve.py
@@ -2,4 +2,4 @@ def useless_function():
     """ 
     A function that returns some text
     """
-    return("some text")
+    return("Honestly i'm just learning how to deploy and API but, hey look I DID IT!!!!")
